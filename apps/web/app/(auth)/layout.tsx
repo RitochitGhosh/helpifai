a430066d7666ed2d50f9e258d1b95b0dd7b1065d
RitochitGhosh <ghosh.18.ritochit12@gmail.com>
2025-08-11 15:19:34 +0530

    03: clerk authentication

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function AuthLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <div className="min-h-screen min-w-screen flex flex-col justify-center items-center">
+            {children}
+        </div>
+    )
+}
